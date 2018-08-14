1841322996914a8d08b91c3561a8250197f4e383
Theodore Lief Gannon <tanuki@gmail.com>
2018-08-14 13:09:55 -0700

    Implement juxStoreToWire; concretize JuxStore to Identity

--- src/Parli/Jux/Types.hs
+++ src/Parli/Jux/Types.hs
@@ -1,11 +1,9 @@
--- {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeFamilyDependencies #-}
 module Parli.Jux.Types where
 
 import           Data.Aeson
 import           RIO
 import qualified RIO.HashMap as HM
-import qualified RIO.HashSet as HS
 
 type JuxValue a = (Eq a, Show a, ToJSON a, FromJSON a)
 type JuxLabel a = (JuxValue a, Hashable a, ToJSONKey a, FromJSONKey a)
@@ -20,8 +18,6 @@ class
   getJuxAttributeEntityType :: JuxAttributeType a -> a
   getJuxDataEntityType      :: JuxEntityData a -> a
   getJuxDataAttributeType   :: JuxAttributeData a -> JuxAttributeType a
-  hasJuxEntityData          :: JuxEntityData a -> Bool
-  -- hasJuxAttributeData       :: JuxAttributeData a -> Bool
 
 class
   ( JuxLabel a
@@ -32,36 +28,27 @@ class
   getJuxRequestQuery  :: JuxQueryRequest a -> a
   getJuxResponseQuery :: JuxQueryResponse a -> a
 
--- newtype Marker a = Marker { getMarker :: Proxy a } -- Is this ever needed?
---   deriving (Eq, Ord, Show, Functor, Generic)
---   deriving newtype (ToJSON, FromJSON)
-
--- newtype Content a = Content { getContent :: a }
---   deriving (Eq, Ord, Show, Functor, Generic)
---   deriving newtype (ToJSON, FromJSON)
-
-
 type JuxRawId = Text
 type JuxIdMap a b f = HashMap (JuxId a) (f b)
 
 data JuxId a = JuxId
-  { juxType :: a
-  , juxId   :: JuxRawId
+  { juxType  :: a
+  , juxRawId :: JuxRawId
   } deriving (Eq, Ord, Show, Data, Typeable, Generic, Hashable)
 
-data JuxStore e q f = JuxStore
-  { juxAttributes :: JuxIdMap (JuxAttributeType e) (JuxAttributeData e) f
-  , juxEntities   :: JuxIdMap e (JuxEntityData e) f
-  , juxQueries    :: JuxIdMap q (JuxQueryRequest q) f
-  , juxResponses  :: JuxIdMap q (JuxQueryResponse q) f
-  , juxTypes      :: HashSet (JuxId e)
+data JuxStore e q = JuxStore
+  { juxAttributes :: JuxIdMap (JuxAttributeType e) (JuxAttributeData e) Identity
+  , juxEntities   :: JuxIdMap e (JuxEntityData e) Identity
+  , juxQueries    :: JuxIdMap q (JuxQueryRequest q) Identity
+  , juxResponses  :: JuxIdMap q (JuxQueryResponse q) Identity
+  , juxTypes      :: HashMap JuxRawId e
   } deriving (Generic, Typeable)
-instance (JuxEntityType e, JuxEntityType q) => Semigroup (JuxStore e q f) where
+instance (JuxEntityType e, JuxEntityType q) => Semigroup (JuxStore e q) where
   (JuxStore a1 e1 q1 r1 t1) <> (JuxStore a2 e2 q2 r2 t2)
     = JuxStore -- favor keys in right-hand argument
       (HM.union a2 a1) (HM.union e2 e1)
       (HM.union q2 q1) (HM.union r2 r1)
-      (HS.union t2 t1)
-instance (JuxEntityType e, JuxEntityType q) => Monoid (JuxStore e q f) where
+      (HM.union t2 t1)
+instance (JuxEntityType e, JuxEntityType q) => Monoid (JuxStore e q) where
   mempty = JuxStore mempty mempty mempty mempty mempty
   mappend = (<>)
--- src/Parli/Jux/Wire.hs
+++ src/Parli/Jux/Wire.hs
@@ -7,7 +7,6 @@ import           Data.Aeson
 import           Parli.Jux.Types
 import           RIO
 import qualified RIO.HashMap as HM
-import qualified RIO.HashSet as HS
 
 type JuxWireMap l v = HashMap l (HashMap JuxRawId v)
 data JuxWire e q = JuxWire
@@ -20,25 +19,27 @@ data JuxWire e q = JuxWire
 deriving instance (JuxEntityType e, JuxQueryType q) => ToJSON (JuxWire e q)
 deriving instance (JuxEntityType e, JuxQueryType q) => FromJSON (JuxWire e q)
 
-juxStoreToWire :: (JuxEntityType e, JuxEntityType q)
-  => JuxStore e q Identity -> JuxWire e q
-juxStoreToWire (JuxStore a e q r t) = JuxWire a' e' q' r' t'
+juxStoreToWire :: (JuxEntityType e, JuxEntityType q) => JuxStore e q -> JuxWire e q
+juxStoreToWire (JuxStore a e q r t) = JuxWire a' e' q' r' t
   where
-    a' = undefined
-    e' = undefined
-    q' = undefined
-    r' = undefined
-    t' = undefined
+    a' = HM.fromListWith (<>) . fmap toWireAttributePair . toWirePairs $ a
+    e' = toWireMap e
+    q' = toWireMap q
+    r' = toWireMap r
+    toWireMap :: (JuxLabel l) => JuxIdMap l v Identity -> JuxWireMap l v
+    toWireMap = HM.fromList . toWirePairs
+    toWirePairs = fmap (uncurry embedMap . toWirePair) . HM.toList
+    toWirePair = (juxType &&& juxRawId) *** runIdentity
+    embedMap (l, k) = (l,) . HM.singleton k
+    toWireAttributePair = getJuxAttributeEntityType . fst &&& uncurry HM.singleton
 
-juxWireToStore :: (JuxEntityType e, JuxEntityType q)
-  => JuxWire e q -> JuxStore e q Identity
-juxWireToStore (JuxWire a' e' q' r' t') = JuxStore a e q r t
+juxWireToStore :: (JuxEntityType e, JuxEntityType q) => JuxWire e q -> JuxStore e q
+juxWireToStore (JuxWire a e q r t) = JuxStore a' e' q' r' t
   where
-    a = toJuxIdMap . fold . HM.elems $ a'
-    e = toJuxIdMap e'
-    q = toJuxIdMap q'
-    r = toJuxIdMap r'
-    t = HS.fromList . fmap (uncurry $ flip JuxId) $ HM.toList t'
-    toJuxIdMap :: (JuxLabel l) => JuxWireMap l v -> JuxIdMap l v Identity
-    toJuxIdMap = HM.fromList . concatMap (uncurry toJuxId) . HM.toList
-    toJuxId k = fmap (JuxId k *** Identity) . HM.toList
+    a' = toIdMap . fold . HM.elems $ a
+    e' = toIdMap e
+    q' = toIdMap q
+    r' = toIdMap r
+    toIdMap :: (JuxLabel l) => JuxWireMap l v -> JuxIdMap l v Identity
+    toIdMap = HM.fromList . concatMap (uncurry toIdPair) . HM.toList
+    toIdPair k = fmap (JuxId k *** Identity) . HM.toList
