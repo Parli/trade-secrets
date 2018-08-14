a1041a9d6b17546401d8585debd29dd8148a7e01
Theodore Lief Gannon <tanuki@gmail.com>
2018-08-13 17:31:37 -0700

    Implement juxWireToStore

--- src/Parli/Jux/Wire.hs
+++ src/Parli/Jux/Wire.hs
@@ -3,31 +3,42 @@ module Parli.Jux.Wire
 ( JuxWire, juxStoreToWire, juxWireToStore
 ) where
 
-import Data.Aeson
-import Parli.Jux.Types
-import RIO
+import           Data.Aeson
+import           Parli.Jux.Types
+import           RIO
+import qualified RIO.HashMap as HM
+import qualified RIO.HashSet as HS
 
-type JuxRawIdMap a f = HashMap JuxRawId (f a)
-type JuxWireObjectMap a b f = HashMap a (JuxRawIdMap b f)
-type JuxWireAttributeMap a f
-  = HashMap a (HashMap (JuxAttributeType a) (JuxRawIdMap (JuxAttributeData a) f))
-type JuxWireConstraint e q f =
-  ( Functor f, JuxEntityType e, JuxQueryType q
-  , JuxValue (f (JuxEntityData e)), JuxValue (f (JuxAttributeData e))
-  , JuxValue (f (JuxQueryRequest q)), JuxValue (f (JuxQueryResponse q))
-  )
-data JuxWire e q f = JuxWire
-  { attributes :: JuxWireAttributeMap e f
-  , entities   :: JuxWireObjectMap e (JuxEntityData e) f
-  , queries    :: JuxWireObjectMap q (JuxQueryRequest q) f
-  , responses  :: JuxWireObjectMap q (JuxQueryResponse q) f
+type JuxWireMap l v = HashMap l (HashMap JuxRawId v)
+data JuxWire e q = JuxWire
+  { attributes :: HashMap e (JuxWireMap (JuxAttributeType e) (JuxAttributeData e))
+  , entities   :: JuxWireMap e (JuxEntityData e)
+  , queries    :: JuxWireMap q (JuxQueryRequest q)
+  , responses  :: JuxWireMap q (JuxQueryResponse q)
   , types      :: HashMap JuxRawId e
   } deriving (Generic, Typeable)
-deriving instance JuxWireConstraint e q f => ToJSON (JuxWire e q f)
-deriving instance JuxWireConstraint e q f => FromJSON (JuxWire e q f)
+deriving instance (JuxEntityType e, JuxQueryType q) => ToJSON (JuxWire e q)
+deriving instance (JuxEntityType e, JuxQueryType q) => FromJSON (JuxWire e q)
 
-juxStoreToWire :: JuxStore e q f -> JuxWire e q f
-juxStoreToWire = undefined
+juxStoreToWire :: (JuxEntityType e, JuxEntityType q)
+  => JuxStore e q Identity -> JuxWire e q
+juxStoreToWire (JuxStore a e q r t) = JuxWire a' e' q' r' t'
+  where
+    a' = undefined
+    e' = undefined
+    q' = undefined
+    r' = undefined
+    t' = undefined
 
-juxWireToStore :: JuxWire e q f -> JuxStore e q f
-juxWireToStore = undefined
+juxWireToStore :: (JuxEntityType e, JuxEntityType q)
+  => JuxWire e q -> JuxStore e q Identity
+juxWireToStore (JuxWire a' e' q' r' t') = JuxStore a e q r t
+  where
+    a = toJuxIdMap . fold . HM.elems $ a'
+    e = toJuxIdMap e'
+    q = toJuxIdMap q'
+    r = toJuxIdMap r'
+    t = HS.fromList . fmap (uncurry $ flip JuxId) $ HM.toList t'
+    toJuxIdMap :: (JuxLabel l) => JuxWireMap l v -> JuxIdMap l v Identity
+    toJuxIdMap = HM.fromList . concatMap (uncurry toJuxId) . HM.toList
+    toJuxId k = fmap (JuxId k *** Identity) . HM.toList
