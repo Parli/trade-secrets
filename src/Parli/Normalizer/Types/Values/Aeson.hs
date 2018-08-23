7c7a755fac63e2713364cf24c251bcdf242e60c3
Theodore Lief Gannon <tanuki@gmail.com>
2018-08-23 14:45:29 -0700

    Correct serialization of subtype enumerations

--- src/Parli/Normalizer/Types/Values/Aeson.hs
+++ src/Parli/Normalizer/Types/Values/Aeson.hs
@@ -16,7 +16,7 @@ deriveNormalizerObjectJSON :: Name -> DecsQ
 deriveNormalizerObjectJSON name = deriveJSON defaultOptions
   { fieldLabelModifier
     = let
-      parts = id &&& dropNamePrefix name
+      parts = id &&& drop (length $ namePrefix name)
       go y x
         | x == "id" = y
         -- | x `elem` ["id", "type"] = y
@@ -26,16 +26,13 @@ deriveNormalizerObjectJSON name = deriveJSON defaultOptions
 
 normalizerEnumToJSON :: Show a => Name -> a -> Value
 normalizerEnumToJSON name x = case words $ show x of
-  []            -> error "Received empty string from Show instance"
-  [constructor] -> String . fromString $ dropNamePrefix name constructor
-  _ : label     -> String . Unsafe.read . unwords $ label
+  []  -> error "Received empty string from Show instance"
+  [c] -> String . fromString . juxLabelToWire $ drop (length $ nameBase name) c
+  _:l -> String . Unsafe.read $ unwords l
 
 normalizerEnumParseJSON :: Read a => (Text -> a) -> Name -> Value -> Parser a
 normalizerEnumParseJSON unknown name = withText (show name) $ \v ->
   either pure pure $ readJuxLabel unknown (fromString (namePrefix name) <> v)
-
-dropNamePrefix :: Name -> String -> String
-dropNamePrefix name = drop $ length (namePrefix name)
 
 namePrefix :: Name -> String
 namePrefix name = juxLabelToWire (nameBase name) <> "_"
