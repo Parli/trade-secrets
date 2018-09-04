7127841b0b51208e243dd78d04671293dc946390
Theodore Lief Gannon <tanuki@gmail.com>
2018-09-04 14:58:31 -0700

    Move Parli.Normalizer(.*) back to canister

--- src/Parli/Normalizer/Types.hs
+++
@@ -1,7 +0,0 @@
-module Parli.Normalizer.Types
-( module X
-) where
-
-import Parli.Normalizer.Types.Environment as X
-import Parli.Normalizer.Types.Values as X
-import Parli.Normalizer.Types.Vocabulary as X
