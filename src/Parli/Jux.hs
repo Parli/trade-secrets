4e887687ef7235aed43d28109e6bd5eb32955fdf
Theodore Lief Gannon <tanuki@gmail.com>
2019-01-14 19:12:47 -0800

    Remove juxStoreDataOfType, add typed data extraction fuctions

--- src/Parli/Jux.hs
+++ src/Parli/Jux.hs
@@ -47,7 +47,7 @@ toAttributeKey a k = k { juxType = a }
 toEntityKey :: (JuxEntityType e, a ~ JuxAttributeType e) => JuxKey a -> JuxKey e
 toEntityKey k@JuxKey{ juxType = a } = k{ juxType = getJuxAttributeEntityType a }
 
--- filtration/extraction
+-- filtration
 juxKeyHasType :: Eq a => a -> JuxKey a -> Bool
 juxKeyHasType t k = juxType k == t
 
@@ -60,14 +60,47 @@ juxMapRestrictIds ks = juxMapFilterKeys $ flip HS.member ks . juxId
 juxMapRestrictTypes :: (Eq a, Hashable a) => HashSet a -> JuxMap a b f -> JuxMap a b f
 juxMapRestrictTypes ts = juxMapFilterKeys $ flip HS.member ts . juxType
 
-juxStoreDataOfType :: Eq a => a -> JuxMap a b Identity -> [b]
-juxStoreDataOfType t = fmap runIdentity . HM.elems . juxMapFilterKeys (juxKeyHasType t)
-
 juxTypesRestrictIds :: HashSet JuxId -> JuxTypes' e -> JuxTypes' e
 juxTypesRestrictIds ks = juxMapFilterKeys $ flip HS.member ks
 
 juxTypesRestrictTypes :: (Eq e, Hashable e) => HashSet e -> JuxTypes' e -> JuxTypes' e
 juxTypesRestrictTypes ts = HM.filter $ flip HS.member ts
+
+-- extraction
+juxMapData :: Eq a => a -> (b -> c) -> JuxMap a b Identity -> [c]
+juxMapData t ex = fmap (ex . runIdentity) . HM.elems . juxMapFilterKeys (juxKeyHasType t)
+juxMapDataPairs :: Eq a => a -> (b -> c) -> JuxMap a b Identity -> [(JuxId, c)]
+juxMapDataPairs t ex = fmap (juxId *** ex . runIdentity) . HM.toList . juxMapFilterKeys (juxKeyHasType t)
+juxMapDataLookup :: (Eq a, Hashable a) => JuxKey a -> (b -> c) -> JuxMap a b Identity -> Maybe c
+juxMapDataLookup k ex = fmap (ex . runIdentity) . HM.lookup k
+
+juxEntitiesData :: JuxEntityType e => e -> (JuxEntityData e -> a) -> JuxStore' e q -> [a]
+juxEntitiesData t ex = juxMapData t ex . juxEntities
+juxEntitiesDataPairs :: JuxEntityType e => e -> (JuxEntityData e -> a) -> JuxStore' e q -> [(JuxId, a)]
+juxEntitiesDataPairs t ex = juxMapDataPairs t ex . juxEntities
+juxEntitiesDataLookup :: JuxEntityType e => JuxKey e -> (JuxEntityData e -> a) -> JuxStore' e q -> Maybe a
+juxEntitiesDataLookup k ex = juxMapDataLookup k ex . juxEntities
+
+juxAttributesData :: JuxEntityType e => JuxAttributeType e -> (JuxAttributeData e -> a) -> JuxStore' e q -> [a]
+juxAttributesData t ex = juxMapData t ex . juxAttributes
+juxAttributesDataPairs :: JuxEntityType e => JuxAttributeType e -> (JuxAttributeData e -> a) -> JuxStore' e q -> [(JuxId, a)]
+juxAttributesDataPairs t ex = juxMapDataPairs t ex . juxAttributes
+juxAttributesDataLookup :: JuxEntityType e => JuxKey (JuxAttributeType e) -> (JuxAttributeData e -> a) -> JuxStore' e q -> Maybe a
+juxAttributesDataLookup k ex = juxMapDataLookup k ex . juxAttributes
+
+juxQueriesData :: JuxQueryType q => q -> (JuxQueryRequest q -> a) -> JuxStore' e q -> [a]
+juxQueriesData t ex = juxMapData t ex . juxQueries
+juxQueriesDataPairs :: JuxQueryType q => q -> (JuxQueryRequest q -> a) -> JuxStore' e q -> [(JuxId, a)]
+juxQueriesDataPairs t ex = juxMapDataPairs t ex . juxQueries
+juxQueriesDataLookup :: JuxQueryType q => JuxKey q -> (JuxQueryRequest q -> a) -> JuxStore' e q -> Maybe a
+juxQueriesDataLookup k ex = juxMapDataLookup k ex . juxQueries
+
+juxResponsesData :: JuxQueryType q => q -> (JuxQueryResponse q -> a) -> JuxStore' e q -> [a]
+juxResponsesData t ex = juxMapData t ex . juxResponses
+juxResponsesDataPairs :: JuxQueryType q => q -> (JuxQueryResponse q -> a) -> JuxStore' e q -> [(JuxId, a)]
+juxResponsesDataPairs t ex = juxMapDataPairs t ex . juxResponses
+juxResponsesDataLookup :: JuxQueryType q => JuxKey q -> (JuxQueryResponse q -> a) -> JuxStore' e q -> Maybe a
+juxResponsesDataLookup k ex = juxMapDataLookup k ex . juxResponses
 
 -- element operations
 juxLookupType :: JuxStoreType e q => JuxId -> JuxStore' e q -> Maybe e
