e7cb6e70125681e02bc382718c968c5421778a4a
Theodore Lief Gannon <tanuki@gmail.com>
2019-01-31 17:17:48 -0800

    Refactor JuxUnwrap to allow Functor instance

--- src/Parli/Jux/Unwrap.hs
+++ src/Parli/Jux/Unwrap.hs
@@ -1,39 +1,36 @@
 module Parli.Jux.Unwrap where
 
 import RIO
-import qualified RIO.Set as S
+import qualified RIO.Map as M
 import Parli.Jux.Core.Types
 import Data.Monoid (Alt(..))
 
-data JuxUnwrap' a b c where
-  JuxUnwrap' :: JuxLabel a =>
-    { juxUnwrapType :: a
-    , juxUnwrapper  :: (b -> c)
-    } -> JuxUnwrap' a b c
-instance Eq (JuxUnwrap' a b c) where JuxUnwrap' t _ == JuxUnwrap' u _ = t == u
-instance Ord (JuxUnwrap' a b c) where JuxUnwrap' t _ <= JuxUnwrap' u _ = t <= u
-
-newtype JuxUnwrap a b c = JuxUnwrap { getJuxUnwrap :: Set (JuxUnwrap' a b c) }
+newtype JuxUnwrap a b c = JuxUnwrap { getJuxUnwrap :: Map a (b -> c) }
   deriving newtype (Monoid, Semigroup)
+instance Functor (JuxUnwrap a b) where
+  fmap f u = JuxUnwrap $ (f .) <$> getJuxUnwrap u
 type JuxUnwrapEntity' e a = JuxUnwrap e (JuxEntityData e) a
 type JuxUnwrapAttribute' e a = JuxUnwrap (JuxAttributeType e) (JuxAttributeData e) a
 type JuxUnwrapQuery' q a = JuxUnwrap q (JuxQueryRequest q) a
 type JuxUnwrapResponse' q a = JuxUnwrap q (JuxQueryResponse q) a
 
 mkJuxUnwrap :: JuxLabel a => a -> (b -> c) -> JuxUnwrap a b c
-mkJuxUnwrap t un = JuxUnwrap . S.singleton $ JuxUnwrap' t un
+mkJuxUnwrap t = JuxUnwrap . M.singleton t
 
-juxUnwraps :: JuxLabel a => JuxUnwrap a b c -> [JuxUnwrap' a b c]
-juxUnwraps = S.toList . getJuxUnwrap
+juxUnwrapPairs :: JuxLabel a => JuxUnwrap a b c -> [(a, b -> c)]
+juxUnwrapPairs = M.toList . getJuxUnwrap
+
+juxUnwrappers :: JuxLabel a => JuxUnwrap a b c -> [b -> c]
+juxUnwrappers = M.elems . getJuxUnwrap
 
 juxUnwrapTypes :: JuxLabel a => JuxUnwrap a b c -> [a]
-juxUnwrapTypes = fmap juxUnwrapType . juxUnwraps
+juxUnwrapTypes = M.keys . getJuxUnwrap
 
 juxDataChecking :: JuxLabel a => (b -> a) -> JuxUnwrap a b c -> b -> Maybe c
 juxDataChecking is un d
-  = getAlt . fold . fmap (Alt . tryUnwrap) $ juxUnwraps un
+  = getAlt . fold . fmap (Alt . tryUnwrap) $ juxUnwrapPairs un
   where
-  tryUnwrap (JuxUnwrap' t un') = if is d == t then Just (un' d) else Nothing
+  tryUnwrap (t, un') = if is d == t then Just (un' d) else Nothing
 
 juxDataKeyed :: JuxLabel a => JuxUnwrap a b c -> JuxKey a -> b -> Maybe c
 juxDataKeyed un (JuxKey t _) = juxDataChecking (const t) un
