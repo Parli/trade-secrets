6404317dacd03e01970bb3cc7eabf904790d8525
Theodore Lief Gannon <tanuki@gmail.com>
2019-03-04 14:26:44 -0800

    Better Has-class structure for StatsDEnv

--- src/Parli/StatsD/Types/Environment.hs
+++ src/Parli/StatsD/Types/Environment.hs
@@ -1,17 +1,21 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Parli.StatsD.Types.Environment where
 
 import RIO
+import Control.Lens.TH.RIO
 
 import qualified Network.StatsD.Datadog as StatsD
 
 newtype StatsDEnv = StatsDEnv { getStatsDEnv :: StatsD.DogStatsSettings }
-class HasDogStatsSettings env where
-  dogStatsSettingsL :: Lens' env StatsD.DogStatsSettings
-instance HasDogStatsSettings StatsDEnv where
-  dogStatsSettingsL = lens getStatsDEnv (const StatsDEnv)
 instance Show StatsDEnv where
   show (StatsDEnv (StatsD.DogStatsSettings host port))
     = "StatsD {" <> host <> ":" <> show port <> "}"
+makeRioClassOnly ''StatsDEnv
+class HasDogStatsSettings env where
+  dogStatsSettingsL :: Lens' env StatsD.DogStatsSettings
+instance HasStatsDEnv env => HasDogStatsSettings env where
+  dogStatsSettingsL = statsDEnvL . lens getStatsDEnv (const StatsDEnv)
 
 type MonadStatsD env m =
   ( MonadIO m, MonadReader env m, HasDogStatsSettings env )
